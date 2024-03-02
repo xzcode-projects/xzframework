0817b44c1f8bc8aa5ab37cf0214171d4efea4990
xz <ldwqh0@outlook.com>
2024-03-02 12:07:48 +0800

    添加错误响应

---
+++ web/src/main/java/com/xzframework/web/domain/ErrorResponse.java
@@ -0,0 +1,53 @@
+package com.xzframework.web.domain;
+
+import java.time.ZonedDateTime;
+
+public class ErrorResponse {
+    private final int status;
+    private final ZonedDateTime timestamp;
+    private final String error;
+    private final String message;
+    private final String path;
+
+    public ErrorResponse() {
+        this(500);
+    }
+
+    public ErrorResponse(int status, String error, String message, String path) {
+        this(status, ZonedDateTime.now(), null, null, null);
+    }
+
+    public ErrorResponse(int status) {
+        this(status, ZonedDateTime.now(), null, null, null);
+    }
+
+    public ErrorResponse(int status, ZonedDateTime timestamp, String error, String message, String path) {
+        this.status = status;
+        this.timestamp = timestamp;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+
+}
