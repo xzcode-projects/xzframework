691aa14277d8d32abac5bae74667ebaceb1ce9ba
xz <ldwqh0@outlook.com>
2024-04-05 02:47:54 +0800

    搬迁一个自定义的UUID生成器

---
+++ data-jpa/src/main/java/org/xzframework/data/id/Helper.java
@@ -0,0 +1,105 @@
+package org.xzframework.data.id;
+
+import java.net.InetAddress;
+
+public final class Helper {
+    private Helper() {
+    }
+
+    // IP ADDRESS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    private static final byte[] ADDRESS_BYTES;
+    private static final int ADDRESS_INT;
+    private static final String ADDRESS_HEX_STRING;
+
+    static {
+        byte[] address;
+        try {
+            address = InetAddress.getLocalHost().getAddress();
+        }
+        catch ( Exception e ) {
+            address = new byte[4];
+        }
+        ADDRESS_BYTES = address;
+        ADDRESS_INT = BytesHelper.toInt( ADDRESS_BYTES );
+        ADDRESS_HEX_STRING = format( ADDRESS_INT );
+    }
+
+    public static byte[] getAddressBytes() {
+        return ADDRESS_BYTES;
+    }
+
+    public static int getAddressInt() {
+        return ADDRESS_INT;
+    }
+
+    public static String getAddressHexString() {
+        return ADDRESS_HEX_STRING;
+    }
+
+
+    // JVM identifier ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    private static final byte[] JVM_IDENTIFIER_BYTES;
+    private static final int JVM_IDENTIFIER_INT;
+    private static final String JVM_IDENTIFIER_HEX_STRING;
+
+    static {
+        JVM_IDENTIFIER_INT = (int) ( System.currentTimeMillis() >>> 8 );
+        JVM_IDENTIFIER_BYTES = BytesHelper.fromInt( JVM_IDENTIFIER_INT );
+        JVM_IDENTIFIER_HEX_STRING = format( JVM_IDENTIFIER_INT );
+    }
+
+    public static byte[] getJvmIdentifierBytes() {
+        return JVM_IDENTIFIER_BYTES;
+    }
+
+    public static int getJvmIdentifierInt() {
+        return JVM_IDENTIFIER_INT;
+    }
+
+    public static String getJvmIdentifierHexString() {
+        return JVM_IDENTIFIER_HEX_STRING;
+    }
+
+
+    // counter ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    private static short counter = (short) 0;
+
+    /**
+     * Unique in a millisecond for this JVM instance
+     * (unless there are more than {@value Short#MAX_VALUE}
+     * instances created in a millisecond)
+     */
+    public static short getCountShort() {
+        synchronized ( Helper.class ) {
+            if ( counter < 0 ) {
+                counter = 0;
+            }
+            return counter++;
+        }
+    }
+
+    public static byte[] getCountBytes() {
+        return BytesHelper.fromShort( getCountShort() );
+    }
+
+
+    // Helper methods ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    public static String format(int value) {
+        final String formatted = Integer.toHexString( value );
+        StringBuilder buf = new StringBuilder( "00000000" );
+        buf.replace( 8 - formatted.length(), 8, formatted );
+        return buf.toString();
+    }
+
+    public static String format(short value) {
+        String formatted = Integer.toHexString( value );
+        StringBuilder buf = new StringBuilder( "0000" );
+        buf.replace( 4 - formatted.length(), 4, formatted );
+        return buf.toString();
+    }
+
+}
---
+++ data-jpa/src/main/java/org/xzframework/data/id/UuidGenerator.java
@@ -0,0 +1,44 @@
+package org.xzframework.data.id;
+
+import java.util.UUID;
+
+public class UuidGenerator {
+    private final long mostSignificantBits;
+
+    private static final UuidGenerator INSTANCE = new UuidGenerator();
+
+    private UuidGenerator() {
+        // generate the "most significant bits" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        byte[] hiBits = new byte[8];
+        // use address as first 32 bits (8 * 4 bytes)
+        System.arraycopy(Helper.getAddressBytes(), 0, hiBits, 0, 4);
+        // use the "jvm identifier" as the next 32 bits
+        System.arraycopy(Helper.getJvmIdentifierBytes(), 0, hiBits, 4, 4);
+        // set the version (rfc term) appropriately
+        hiBits[6] &= 0x0f;
+        hiBits[6] |= 0x10;
+        mostSignificantBits = BytesHelper.asLong(hiBits);
+    }
+
+    public static long generateLeastSignificantBits(long seed) {
+        byte[] loBits = new byte[8];
+        short hiTime = (short) (seed >>> 32);
+        int loTime = (int) seed;
+        System.arraycopy(BytesHelper.fromShort(hiTime), 0, loBits, 0, 2);
+        System.arraycopy(BytesHelper.fromInt(loTime), 0, loBits, 2, 4);
+        System.arraycopy(Helper.getCountBytes(), 0, loBits, 6, 2);
+        loBits[0] &= 0x3f;
+        loBits[0] |= (byte) ((byte) 2 << (byte) 6);
+
+        return BytesHelper.asLong(loBits);
+    }
+
+    public UUID generateUuid() {
+        long leastSignificantBits = generateLeastSignificantBits(System.currentTimeMillis());
+        return new UUID(mostSignificantBits, leastSignificantBits);
+    }
+
+    public UUID randomUuid() {
+        return INSTANCE.randomUuid();
+    }
+}
