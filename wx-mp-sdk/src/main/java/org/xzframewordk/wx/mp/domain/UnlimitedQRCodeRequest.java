5bdcfee313dce8bda7cfe2abc5e8d0cd693bc8b4
xz <ldwqh0@outlook.com>
2024-05-14 16:35:20 +0800

    修改发送短信的参数配置

--- wx-mp-sdk/src/main/java/org/xzframewordk/wx/mp/domain/UnlimitedQRCodeRequest.java
+++ wx-mp-sdk/src/main/java/org/xzframewordk/wx/mp/domain/UnlimitedQRCodeRequest.java
@@ -1,35 +1,27 @@
 package org.xzframewordk.wx.mp.domain;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public record UnlimitedQRCodeRequest(
-        @RequestParam(value = "scene", required = false)
         String scene,
 
-        @RequestParam(value = "page", required = false)
         String page,
 
         @JsonGetter("check_path")
-        @RequestParam(value = "check_path", defaultValue = "false", required = false)
         Boolean checkPath,
 
-        @RequestParam(value = "width", defaultValue = "140", required = false)
         int width,
 
         @JsonGetter("auto_color")
-        @RequestParam(value = "auto_color", defaultValue = "false", required = false)
         Boolean autoColor,
 
         @JsonGetter("line_color")
         QrCodeColor lineColor,
 
         @JsonGetter("is_hyaline")
-        @RequestParam(value = "is_hyaline", defaultValue = "false", required = false)
         Boolean hyaline,
 
         @JsonGetter("env_version")
-        @RequestParam(value = "env_version", defaultValue = "release", required = false)
         String envVersion
 ) {
 
