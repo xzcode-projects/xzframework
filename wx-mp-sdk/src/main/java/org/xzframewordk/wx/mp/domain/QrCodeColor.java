9ef925ef22a53e3d964bde821ff68520cf9eca07
xz <ldwqh0@outlook.com>
2024-05-14 18:04:40 +0800

    修改导出图标颜色参数

--- wx-mp-sdk/src/main/java/org/xzframewordk/wx/mp/domain/QrCodeColor.java
+++ wx-mp-sdk/src/main/java/org/xzframewordk/wx/mp/domain/QrCodeColor.java
@@ -1,8 +1,13 @@
 package org.xzframewordk.wx.mp.domain;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+
 public record QrCodeColor(
+        @JsonGetter("r")
         Integer red,
+        @JsonGetter("g")
         Integer green,
+        @JsonGetter("b")
         Integer blue
 ) {
 }
