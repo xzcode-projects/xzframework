cf7d9313a3072228567c8c2fa0986f1f756349d6
xz <ldwqh0@outlook.com>
2024-05-27 19:06:21 +0800

    升级spring-boot版本, 修改构建配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,11 +18,11 @@ include("xzframework-dependencies")
 
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.9.23"
-        kotlin("kapt") version "1.9.23"
-        kotlin("plugin.spring") version "1.9.23"
-        kotlin("plugin.jpa") version "1.9.23"
-        id("org.hibernate.orm") version "6.5.0.Final"
+        kotlin("jvm") version "1.9.24"
+        kotlin("kapt") version "1.9.24"
+        kotlin("plugin.spring") version "1.9.24"
+        kotlin("plugin.jpa") version "1.9.24"
+        id("org.hibernate.orm") version "6.4.8.Final"
         id("org.springframework.boot") version "3.2.6"
         id("cz.habarta.typescript-generator") version "3.2.1263"
     }
