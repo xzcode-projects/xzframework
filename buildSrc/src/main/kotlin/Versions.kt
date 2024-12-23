d034583baa24189ee7cb41dc4d9ac349c3b375f2
xz <ldwqh0@outlook.com>
2024-12-23 17:56:10 +0800

    升级spring 版本

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,5 @@
-import java.util.*
-
-val versions = Properties().apply {
-    val stream = object {}.javaClass.getResourceAsStream("versions.properties")
-    load(stream)
-}
+val versions =  mapOf(
+    "spring_boot" to "3.4.0"
+)
 
 
