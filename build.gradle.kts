7095b75877b0fedb0fdb3c948ba9893a479837fa
xz <ldwqh0@outlook.com>
2025-02-09 13:07:01 +0800

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,3 @@
 allprojects {
-    version = "3.4.2-SNAPSHOT"
+    version = "3.4.1-SNAPSHOT"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ pluginManagement {
         kotlin("plugin.spring") version "1.9.25"
         kotlin("plugin.jpa") version "1.9.25"
         id("org.hibernate.orm") version "6.6.4.Final"
-        id("org.springframework.boot") version "3.4.2"
+        id("org.springframework.boot") version "3.4.1"
         id("cz.habarta.typescript-generator") version "3.2.1263"
     }
 }
