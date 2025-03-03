59b623b9aac5adee12afd10cfdd598f84344b275
xz <ldwqh0@outlook.com>
2025-03-03 23:23:26 +0800

    添加querydsl分页配置

--- data-jpa/build.gradle.kts
+++ data-jpa/build.gradle.kts
@@ -10,8 +10,9 @@ dependencies {
     compileOnly("jakarta.persistence:jakarta.persistence-api")
     compileOnly("com.querydsl:querydsl-core")
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
+    compileOnly("com.querydsl", "querydsl-jpa", "5.1.0", classifier = "jakarta")
 
-    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.1.0:jakarta")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api:3.1.0")
 }
 
---
+++ data-jpa/src/main/kotlin/org/xzframework/data/querydsl/JPQLQueryExt.kt
@@ -0,0 +1,82 @@
+package org.xzframework.data.querydsl
+
+import com.querydsl.core.types.Expression
+import com.querydsl.core.types.Order
+import com.querydsl.core.types.OrderSpecifier
+import com.querydsl.core.types.Path
+import com.querydsl.core.types.dsl.Expressions
+import com.querydsl.core.types.dsl.PathBuilder
+import com.querydsl.core.types.dsl.PathBuilderFactory
+import com.querydsl.jpa.JPQLQuery
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.mapping.PropertyPath
+import org.springframework.data.querydsl.QSort
+
+
+fun <T> JPQLQuery<T>.applyPagination(pageable: Pageable, clazz: Class<T>): JPQLQuery<T> {
+    if (pageable.isUnpaged) {
+        return this
+    }
+    val pathBuilder = PathBuilderFactory().create(clazz)
+    offset(pageable.offset)
+    limit(pageable.pageSize.toLong())
+    applySorting(this, pageable.sort, pathBuilder)
+    return this
+}
+
+fun <T> JPQLQuery<T>.applySorting(sort: Sort, clazz: Class<T>): JPQLQuery<T> {
+    val pathBuilder = PathBuilderFactory().create(clazz)
+    applySorting(this, sort, pathBuilder)
+    return this
+}
+
+private fun <T> applySorting(query: JPQLQuery<T>, sort: Sort, builder: PathBuilder<*>) {
+    if (sort.isSorted) {
+        if (sort is QSort) {
+            addOrderByFrom(query, sort)
+        }
+        addOrderByFrom(query, sort, builder)
+    }
+}
+
+private fun <T> addOrderByFrom(query: JPQLQuery<T>, sort: Sort, builder: PathBuilder<*>) {
+    for (order in sort) {
+        query.orderBy(toOrderSpecifier(order, builder))
+    }
+}
+
+private fun <T> addOrderByFrom(query: JPQLQuery<T>, qSort: QSort): JPQLQuery<T> {
+    val orderSpecifiers = qSort.orderSpecifiers
+    return query.orderBy(*orderSpecifiers.toTypedArray())
+}
+
+
+private fun toOrderSpecifier(order: Sort.Order, builder: PathBuilder<*>): OrderSpecifier<*> {
+    return OrderSpecifier(
+        if (order.isAscending) Order.ASC else Order.DESC,
+        buildOrderPropertyPathFrom(order, builder),
+        toQueryDslNullHandling(order.nullHandling)
+    )
+}
+
+private fun <T : Comparator<*>> buildOrderPropertyPathFrom(order: Sort.Order, builder: PathBuilder<*>): Expression<T> {
+    var path: PropertyPath? = PropertyPath.from(order.property, builder.type!!)
+    var sortPropertyExpression: Expression<*> = builder
+    while (path != null) {
+        sortPropertyExpression = if (!path.hasNext() && order.isIgnoreCase && String::class.java == path.type)
+            Expressions.stringPath(sortPropertyExpression as Path<*>, path.segment).lower() //
+        else
+            Expressions.path(path.type, sortPropertyExpression as Path<*>, path.segment)
+        path = path.next()
+    }
+    return sortPropertyExpression as Expression<T>
+}
+
+private fun toQueryDslNullHandling(nullHandling: Sort.NullHandling): OrderSpecifier.NullHandling {
+    return when (nullHandling) {
+        Sort.NullHandling.NULLS_FIRST -> OrderSpecifier.NullHandling.NullsFirst
+        Sort.NullHandling.NULLS_LAST -> OrderSpecifier.NullHandling.NullsLast
+        else -> OrderSpecifier.NullHandling.Default
+    }
+}
