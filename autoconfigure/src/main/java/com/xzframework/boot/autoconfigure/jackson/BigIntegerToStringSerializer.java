6fd51c4980105e4b6ea0ffd9028bca26b2cf0699
xz <ldwqh0@outlook.com>
2025-09-14 01:33:11 +0800

    修改几种大数据的序列化方式。当超限时，使用String序列化

--- autoconfigure/src/main/java/com/xzframework/boot/autoconfigure/jackson/BigIntegerToStringSerializer.java
+++ autoconfigure/src/main/java/com/xzframework/boot/autoconfigure/jackson/BigIntegerToStringSerializer.java
@@ -12,9 +12,9 @@ public class BigIntegerToStringSerializer extends JsonSerializer<BigInteger> {
     public static final BigIntegerToStringSerializer instance = new BigIntegerToStringSerializer();
 
     //    private static final Long MAX_SAFE_INTEGER = 9007199254740991L;
-    private static final BigInteger MAX_SAFE_INTEGER = new BigInteger("9000000000000000L");
+    private static final BigInteger MAX_SAFE_INTEGER = new BigInteger("9000000000000000");
     //    private static final Long MIN_SAFE_INTEGER = -9007199254740991L;
-    private static final BigInteger MIN_SAFE_INTEGER = new BigInteger("-9000000000000000L");
+    private static final BigInteger MIN_SAFE_INTEGER = new BigInteger("-9000000000000000");
 
     @Override
     public void serialize(BigInteger value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
