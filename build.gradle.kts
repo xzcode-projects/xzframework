7208ee578eccd68ca9c2c993a451aabf11baed5a
xz <ldwqh0@outlook.com>
2026-03-29 09:20:40 +0800

    升级到spring boot 4.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,3 @@
 allprojects {
-    version = "4.0.4-SNAPSHOT"
+    version = "4.0.5-SNAPSHOT"
 }
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,3 @@
 val versions = mapOf(
-    "spring_boot" to "4.0.4"
+    "spring_boot" to "4.0.5"
 )
