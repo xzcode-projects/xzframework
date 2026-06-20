2334839a28458ccf0eef5e967151220aef93848f
xz <ldwqh0@outlook.com>
2026-06-21 00:41:14 +0800

    修改创建人的标注

--- data-jpa/src/main/java/org/xzframework/data/jpa/domain/AbstractAuditEntity.java
+++ data-jpa/src/main/java/org/xzframework/data/jpa/domain/AbstractAuditEntity.java
@@ -1,57 +1,60 @@
 package org.xzframework.data.jpa.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.util.Optional;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractAuditEntity<ID extends Serializable & Comparable<ID>, UID extends Serializable & Comparable<UID>> extends AbstractEntity<ID> {
 
-    @Column(name = "created_user_id_", updatable = false)
-    private UID createdUserId;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(
+                    name = "userid",
+                    column = @Column(name = "created_user_id_", updatable = false)
+            ),
+            @AttributeOverride(
+                    name = "username",
+                    column = @Column(name = "created_user_name_", updatable = false, length = 100)
+            )
+    })
+    private Auditor<UID> createdBy;
 
-    @Column(name = "last_modified_user_id_")
-    private UID lastModifiedUserId;
-
-    @Column(name = "created_user_name_", updatable = false, length = 100)
-    private String createdUserName;
-
-    @Column(name = "last_modified_user_name_", length = 100)
-    private String lastModifiedUserName;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(
+                    name = "userid",
+                    column = @Column(name = "last_modified_user_id_")
+            ),
+            @AttributeOverride(
+                    name = "username",
+                    column = @Column(name = "last_modified_user_name_", length = 100)
+            )
+    })
+    private Auditor<UID> lastModifiedBy;
 
     @NonNull
-    @CreatedBy
     public Optional<Auditor<UID>> getCreatedBy() {
-        return Optional.ofNullable(createdUserId)
-                .map(uid -> new Auditor<>(uid, createdUserName));
+        return Optional.ofNullable(createdBy);
     }
 
     @CreatedBy
     public void setCreatedBy(@NonNull Auditor<UID> createdBy) {
-        this.createdUserId = createdBy.getUserid();
-        this.createdUserName = createdBy.getUsername();
+        this.createdBy = createdBy;
     }
 
     @NonNull
-    @LastModifiedBy
     public Optional<Auditor<UID>> getLastModifiedBy() {
-        return Optional.ofNullable(lastModifiedUserId).map(
-                uid -> new Auditor<>(uid, lastModifiedUserName)
-        );
+        return Optional.ofNullable(lastModifiedBy);
     }
 
     @LastModifiedBy
     public void setLastModifiedBy(@NonNull Auditor<UID> lastModifiedBy) {
-        this.lastModifiedUserId = lastModifiedBy.getUserid();
-        this.lastModifiedUserName = lastModifiedBy.getUsername();
+        this.lastModifiedBy = lastModifiedBy;
     }
 
 }
